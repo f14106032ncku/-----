0f8c968522146d59eb4feaa297c4b7fed64a3afa
f14106032 <f14106032@gs.ncuk.edu.tw>
2024-12-13 18:54:11 +0800

    加入<=

--- IDEX.v
+++ IDEX.v
@@ -20,8 +20,8 @@ module IDEX(
   output reg [31:0] readdata1_EX, //2bit mux
   output reg [31:0] readdata2_EX, //2bit mux
   output reg [6:0] opcode_EX,
-  output [2:0] fun3_EX,
-  output [6:0] fun7_EX,
+  output reg [2:0] fun3_EX,
+  output reg [6:0] fun7_EX,
   output reg branch_EX,memread_EX,memtoreg_EX, memwrite_EX, regwrite_EX,alusrc_EX
 );
   
@@ -29,37 +29,41 @@ module IDEX(
     begin
       if (rst || flush)
         begin
-          pc_EX = 31'b0;
-          rs1_EX = 5'b0;
-          rs2_EX = 5'b0;
-          rd_EX = 5'b0;
-          imm_data_EX = 31'b0;
-          readdata1_EX = 31'b0;
-          readdata2_EX = 31'b0;
-          opcode_EX = 4'b0;
-          branch_EX = 1'b0;
-          memread_EX = 1'b0;
-          memtoreg_EX =1'b0;
-          memwrite_EX = 1'b0;
-          regwrite_EX = 1'b0;
-          alusrc_EX = 1'b0;
+          pc_EX <= 31'b0;
+          rs1_EX <= 5'b0;
+          rs2_EX <= 5'b0;
+          rd_EX <= 5'b0;
+          imm_data_EX <= 31'b0;
+          readdata1_EX <= 31'b0;
+          readdata2_EX <= 31'b0;
+          opcode_EX <= 7'b0;
+          fun3_EX <= 4'b0;    
+          fun7_EX <= 7'b0;                 
+          branch_EX <= 1'b0;
+          memread_EX <= 1'b0;
+          memtoreg_EX <=1'b0;
+          memwrite_EX <= 1'b0;
+          regwrite_EX <= 1'b0;
+          alusrc_EX <= 1'b0;
         end
       else
         begin
-          pc_EX = pc_ID;
-          rs1_EX = rs1_ID;
-          rs2_EX = rs2_ID;
-          rd_EX = rd_ID;
-          imm_data_EX = imm_data_ID;
-          readdata1_EX = readdata1_ID;
-          readdata2_EX = readdata2_ID;
-          opcode_EX = opcode_ID; //when connecting in top module Funct4 is wire containing this section of 31 bit instruction {instruction[30],instruction[14:12]}
-          branch_EX = branch_ID;
-          memread_EX = memread_ID;
-          memtoreg_EX = memtoreg_ID;
-          memwrite_EX = memwrite_ID;
-          regwrite_EX = regwrite_ID;
-          alusrc_EX = alusrc_ID;     
+          pc_EX <= pc_ID;
+          rs1_EX <= rs1_ID;
+          rs2_EX <= rs2_ID;
+          rd_EX <= rd_ID;
+          imm_data_EX <= imm_data_ID;
+          readdata1_EX <= readdata1_ID;
+          readdata2_EX <= readdata2_ID;
+          opcode_EX <= opcode_ID;
+          fun3_EX <= fun3_ID;    
+          fun7_EX <= fun7_ID;            
+          branch_EX <= branch_ID;
+          memread_EX <= memread_ID;
+          memtoreg_EX <= memtoreg_ID;
+          memwrite_EX <= memwrite_ID;
+          regwrite_EX <= regwrite_ID;
+          alusrc_EX <= alusrc_ID;     
         end
     end
 endmodule
