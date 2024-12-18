78393ec6a2581f233b1194b4b35e5c916fcb8ecc
frank0988 <84711657+frank0988@users.noreply.github.com>
2024-12-18 16:13:01 +0800

    Update memory.v

--- memory.v
+++ memory.v
@@ -2,9 +2,10 @@ module mem(
   input clk ,
   input wen,
   input [31:0] wdata ,addr,
-  output[31:0] rdata
+  output reg [31:0] rdata
 );
-  reg [7:0] mem[31:0]; 
+  reg [31:0] mem[31:0]; 
+  /*
   always @(posedge clk)
     begin
       if (wen)
@@ -14,11 +15,29 @@ module mem(
           mem[addr+2] <= wdata[23:16];
           mem[addr+3] <= wdata[31:24];
         end
+    end */
+    always @ (*)
+      begin
+        if (wen)
+          begin
+            mem[addr]   <= wdata;
+          end
+      end
+/*
+    initial begin
+      $monitor("addr:  %d", addr);      
     end
-	
-assign rdata[7:0]   = mem[addr];
+*/
+
+              	
+ always @ (*)
+    begin
+          rdata = mem[addr];
+    end
+    /*
+assign rdata[7:0]  = mem[addr];
 assign rdata[15:8]  = mem[addr+1];
 assign rdata[23:16] = mem[addr+2];
 assign rdata[31:24] = mem[addr+3];	
-	
+	*/
 endmodule
